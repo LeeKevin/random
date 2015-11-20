a3c6e149dac96413d9722e670c8bf3f1d79947fc
Kevin Lee <lee.kev@me.com>
2015-11-20 16:20:50 -0700

    Check todos bash script. I'm usually forgetful and this helps a lot!

---
+++ checkTodos.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+COLOR='\033[0;33m'
+NC='\033[0m' # No Color
+
+for FILE in `git diff HEAD --staged --name-only`; do
+    result=$(grep -n --color=always 'TODO' $FILE 2>&1) 
+    if [ $? -eq 0 ]; then
+        echo ${COLOR}$FILE 'contains TODO:'${NC}
+        echo "$result"
+        exec </dev/tty
+        read -p "Skip? (Y/N) : " skip
+        exec <&-
+        if [  "$skip" = 'Y'  ] || [  "$skip" = 'y'  ]; then
+            continue
+        fi
+        exit 1
+    fi
+done
+exit
